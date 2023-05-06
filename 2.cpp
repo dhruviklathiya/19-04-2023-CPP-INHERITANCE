cf367e164289a8ee129781ddbf6d030bf6a78f1a
DHRUVIK LATHIYA <115994798+dhruviklathiya@users.noreply.github.com>
2023-05-06 14:52:15 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -3,38 +3,38 @@ using namespace std;
 class V1{
 	public:
 	int a;
-	 V1(){
-	 	cout<<"Enter value 1 : ";
-	 	cin>>a;
+	V1(){
+	cout<<"Enter value 1 : ";
+	cin>>a;
 	}
 };
 class V2 : public V1{
 	public:
-		int b;
-	 V2(){
-	 	cout<<"Enter value 2 : ";
-	 	cin>>b;
+	int b;
+	V2(){
+	cout<<"Enter value 2 : ";
+	cin>>b;
 	}
 };
 class V3  {
 	public:
 		int c;
-	 V3(){
-	 	cout<<"Enter value 3 : ";
-	 	cin>>c;
+	V3(){
+	cout<<"Enter value 3 : ";
+	cin>>c;
 	}
 };
 class V4:public V2 , public V3{
 	public:
 		int d,e;
-	 V4(){
-	 	cout<<"Enter value 4 : ";
-	 	cin>>d;
-        e = a+b+c+d;
-		cout<<endl<<"Addition of all 4 value is = "<<e;
+	V4(){
+	cout<<"Enter value 4 : ";
+	cin>>d;
+    e = a+b+c+d;
+	cout<<endl<<"Addition of all 4 value is = "<<e;
 	}
 };
 int main(){
-      V4 obj;	
+    V4 obj;	
 	return 0;
 }
